f060e774f8e860033a82ddaa201faa614fb071b9
horacio <horaciob@gmail.com>
2014-02-01 10:56:41 -0400

    separe get and set as different functions, adding spread key read and write

--- memcachedping.c
+++ memcachedping.c
@@ -3,17 +3,49 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdlib.h>
 
+
+#define SPREAD_GET_RANGE 10
+#define SPREAD_WRITE_RANGE 10
 long size=10000;
 
 
-int fill_value(char *value,int size){
+int fill_value(char *server,int port,int size,int spread_range){
+  memcached_server_st *servers = NULL;
+  memcached_st *memc;
+  memcached_return rc;
+
+  char value[1000000];  //The default maximum object size is 1MB in memcached
+  char key[20];
   int i=0;
-  for (i=0; i < size; i++){
-    value[i]='A';
+  
+  memc = memcached_create(NULL);
+
+  servers = memcached_server_list_append(servers, server, port, &rc);
+  rc = memcached_server_push(memc, servers);
+  
+  if (rc != MEMCACHED_SUCCESS){
+    fprintf(stderr, "Couldn't add server for write: %s\n", memcached_strerror(memc, rc));
+    exit(1);
   }
-  value[i]='\0';
+
+  for( ; spread_range>=0; spread_range--){
+    for (i=0; i < size; i++){
+      value[i]='A';
+    }
+    value[i]='\0';
+    sprintf(key,"test%d",spread_range);
+    rc = memcached_set(memc, key, strlen(key), value, strlen(value), (time_t)0, MEMCACHED_BEHAVIOR_TCP_NODELAY);
+
+    if (rc != MEMCACHED_SUCCESS){
+      fprintf(stderr, "Couldn't store key: %s\n", memcached_strerror(memc, rc));
+      exit(1);
+    }
+  }
+
   return 0;
+  
 }
 
 
@@ -27,15 +59,12 @@ double get_wall_time() {
   return ((double)t.tv_sec + (double) t.tv_nsec/1000000000)*1000;
 }
 
+int start_get_test(char *server,int port, int package_size,int spread_range) {
 
-int main(int argc, char **argv) {
   memcached_server_st *servers = NULL;
   memcached_st *memc;
   memcached_return rc;
-  char *key = "test";
-  char value[1000000];  //The default maximum object size is 1MB in memcached
-  char server[50];
-  int  port=0;
+  char key[20];
   long errors_count=0,succesfull_count=0,count=size;
   char *retrieved_value;
   size_t value_length;
@@ -44,15 +73,9 @@ int main(int argc, char **argv) {
   double avg_timer=0,best_timer=0,worse_timer=0,current_timer=0;
   int first_flag,i;
   int segments[5];
-  int package_size=0,real_size=0;
-  strncpy(server,argv[1],strlen(argv[1])+1);
-  port=atoi(argv[2]);
+  int real_size=0;
   memc = memcached_create(NULL);
    
-  package_size=atoi(argv[3]);
-  
-  fill_value(value,package_size);  
-
   printf("Server: %s \nPort: %d\n size in Bytes:%d\n ",server,port,package_size);
 
   servers = memcached_server_list_append(servers, server, port, &rc);
@@ -63,12 +86,11 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  rc = memcached_set(memc, key, strlen(key), value, strlen(value), (time_t)0, MEMCACHED_BEHAVIOR_TCP_NODELAY);
-
-  if (rc != MEMCACHED_SUCCESS){
-    fprintf(stderr, "Couldn't store key: %s\n", memcached_strerror(memc, rc));
-    exit(1);
-  }
+  if(memcached_behavior_set(memc,MEMCACHED_BEHAVIOR_USE_UDP,1) != MEMCACHED_SUCCESS)
+    perror("Memcached setting UDP: "); 
+  
+  printf("UDP FLAG SET AS: %d\n",(int)memcached_behavior_get(memc,MEMCACHED_BEHAVIOR_USE_UDP));
+   
 
   flags=MEMCACHED_BEHAVIOR_TCP_NODELAY;
   while(1){
@@ -79,12 +101,14 @@ int main(int argc, char **argv) {
     best_timer=1000;
     worse_timer=0; 
     current_timer=0;
+    errors_count=0;
     
     for(i=0;i<5;i++){
       segments[i]=0;
     }
 
     while(count){
+      sprintf(key,"test%d", rand() % spread_range);
       first_timer=get_wall_time();
       retrieved_value = memcached_get(memc, key, strlen(key), &value_length, &flags, &rc);
       second_timer=get_wall_time();
@@ -134,5 +158,20 @@ int main(int argc, char **argv) {
 
     fflush(stdout);
   }
+  return 0;
+
+}
+int main(int argc, char **argv) {
+  char server[100];
+  int  port=0;
+  int package_size=0;
+  strncpy(server,argv[1],strlen(argv[1])+1);
+  port=atoi(argv[2]);
+  package_size=atoi(argv[3]);
+
+
+  fill_value(server,port,package_size,SPREAD_WRITE_RANGE);  
+
+  start_get_test(server,port,package_size,SPREAD_GET_RANGE);
   return 0;
 }
